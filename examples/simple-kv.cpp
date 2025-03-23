20fcace0d76cea1fac96ea650e46fc14ceb5e083
darryl west <darryl.west@raincitysoftware.com>
2025-03-23 06:54:35 -0700

    example

--- examples/simple-kv.cpp
+++ examples/simple-kv.cpp
@@ -1,34 +1,36 @@
 
 
 
+#include <quickkv/quickkv.hpp>
 #include <print>
-#include <iostream>
-#include "quickkv/quickkv.hpp" // Adjust this include based on your actual header file
+
+void populate_database(quickkv::KVStore& store, const size_t size = 100) {
+
+    for (size_t i = 0; i < size; ++i) {
+        auto key = std::to_string(i + 1000);
+        auto value = key + ":value";
+
+        store.set(key, value);
+    }
+}
 
 int main() {
     // Create an instance of your key-value store
-    quickkv::KVStore db;
-    std::println("db size: {}", db.size());
+    quickkv::KVStore store;
+    populate_database(store);
 
-    // Insert some key-value pairs
-    db.set("key1", "value1");
-    db.set("key2", "value2");
+    std::println("after fill, db size: {}", store.size());
 
-    Optional<Str> value = db.get("key1");
+
+    const auto keys = store.keys();
+
+    const auto key = keys[20];
+    Optional<Str> value = store.get(key);
     if (value.has_value()) {
-        std::println("key1: {}", value.value());
+        std::println("key {}: {}", key, value.value());
     } else {
         std::println("ERROR key1");
     }
-
-    value = db.get("key2");
-    if (value.has_value()) {
-        std::println("key2: {}", value.value());
-    } else {
-        std::println("ERROR key1");
-    }
-
-    std::println("db size: {}", db.size());
 
     return 0;
 }
